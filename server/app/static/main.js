3406114631dd58361693cdfacc41db3a6fca8835
Harry Payne <payne@nfdc.net>
2015-03-18 22:02:51 -0400

    Restoring a lost main.js.

---
+++ server/app/static/main.js
@@ -0,0 +1,68 @@
+(function() {
+  
+  "use strict";
+  
+  var selectApp = angular.module('SelectApp',[]);
+  
+  selectApp.controller('selectController', ['$scope', '$http', 
+  function($scope, $http){
+    $scope.nameLogic = "or";
+    $scope.finalID = "0";
+    
+    $http.get('/getBriefDescriptions').
+      success(function(results){
+        $scope.projects = results;
+      });
+  }
+  
+  ]);
+  
+  selectApp.filter("nameSearch", function() {
+    return function(projects, searchText, nameLogic, finalID) {
+      /* return everything if no search string */
+      if (!searchText) return projects;
+
+      var projlen = projects.length || 0;
+      var st = (searchText || "").toLowerCase();
+      var words = st.split(" ");
+      var wordslen = words.length;
+      var bailout, j;
+      
+      var out = [];
+      
+      for (var i = 0; i < projects.length; i++ ) {
+        if (nameLogic == "phrase") {
+          /* stop if no match */
+          if (!(projects[i].name+" "+projects[i].description).toLowerCase().match(st)) continue;
+        }
+        else if (nameLogic == "and") {
+          /* stop after the first non-match */
+          bailout = false;
+          for ( j = 0; j < wordslen; j++ ) {
+            if (!(projects[i].name+" "+projects[i].description).toLowerCase().match(words[j])) {
+              bailout = true;
+              break;
+            }
+          }
+          if (bailout) continue;
+        }
+        else if (nameLogic == "or") {
+          /* stop after the first match */
+          bailout = true;
+          for ( j = 0; j < wordslen; j++ ) {
+            if ((projects[i].name+" "+projects[i].description).toLowerCase().match(words[j])) {
+              bailout = false;
+              break;
+            }
+          }
+          if (bailout) continue;
+        }
+        if (finalID == "0" && projects[i].finalID != 0) continue;
+        out.push(projects[i]);
+      }
+      
+      return out;
+    };
+  });
+  
+}());
