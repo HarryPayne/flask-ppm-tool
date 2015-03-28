6209f2b96036879de6bb20e07119536beaa505f1
Harry Payne <payne@nfdc.net>
2015-03-28 14:56:46 -0400

    Took out most of the defer/promise workarounds inflicted in the previous solution. I'm still getting an error at $scope.$close(user), where user is the response from assignCurrentUser.

--- server/app/static/loginModal/loginModal.js
+++ server/app/static/loginModal/loginModal.js
@@ -7,38 +7,65 @@
     'app.login'
   ]);
   
-  loginModalCtrl.service('loginModal', function($modal, $rootScope) {
+  loginModalCtrl.service('loginModal', ['$modal', '$rootScope', 'store', 'jwtHelper',
+    function($modal, $rootScope, store, jwtHelper) {
 
-    function assignCurrentUser(user) {
-      //$rootScope.currentUser = user;
-      return user;
+      function assignCurrentUser(response) {
+        if (response.status == 200) {
+          store.set('jwt', response.data.token);
+          var user = jwtHelper.decodeToken(response.data.token);
+          $rootScope.currentUser = user;
+          return user;
+        }
+        return response;
+      };
+
+      return function() {
+        var instance = $modal.open({
+          templateUrl: 'static/loginModal/login.html',
+          controller: 'LoginModalCtrl',
+          controllerAs: 'LoginModalCtrl'
+        });
+
+        return instance.result.then(assignCurrentUser);
+      };
     }
-    
-    return function() {
-      var instance = $modal.open({
-        templateUrl: 'static/loginModal/login.html',
-        controller: 'LoginModalCtrl',
-        controllerAs: 'LoginModalCtrl'
-      });
-      
-      //return instance.result.then(assignCurrentUser);
-      return instance.result;
-    };
-  });
+  ]);
   
-  loginModalCtrl.controller('LoginModalCtrl', function($scope, $rootScope, UsersApi) {
+  loginModalCtrl.controller('LoginModalCtrl', function($scope, UsersApi) {
 
     this.cancel = $scope.$dismiss;
     
-    this.login = function (email, password) {
-      UsersApi.login(email, password);
-      if ($rootScope.currentUser) {
-        $scope.$close();
-      };
+    this.submit = function (email, password) {
+      UsersApi.login(email, password).then(function (user) {
+        $scope.$close(user);
+      });
     };
     
   });
 
+  loginModalCtrl.run(['$rootScope', '$state', 'store', 'jwtHelper',  'loginModal',
+    function ($rootScope, $state, store, jwtHelper, loginModal) {
+      $state.transitionTo('select');
+
+      $rootScope.$on('$stateChangeStart', function(e, to) {
+        var requiresLogin = to.data && to.data.requiresLogin;
+        var noActiveToken = !store.get('jwt') || jwtHelper.isTokenExpired(store.get('jwt'));
+        if (requiresLogin && noActiveToken) {
+          e.preventDefault();
+          
+          loginModal()
+          .then(function () {
+            return $state.go(to.name);
+          })
+          .catch(function () {
+            return $state.go('select');
+          });
+        }
+      });
+    }
+  ]);
+  
   loginModalCtrl.config(function ($httpProvider) {
   
     $httpProvider.interceptors.push(function ($timeout, $q, $injector) {
@@ -65,6 +92,7 @@
               deferred.resolve( $http(rejection.config) );
             })
             .catch(function () {
+              $state.go('select');
               deferred.reject(rejection);
             });
   
@@ -75,41 +103,16 @@
   
   });
 
-  loginModalCtrl.factory('UsersApi', ['$q', '$rootScope', '$http', '$state', 'store', 'jwtHelper', '$stateParams', 
-    function($q, $rootScope, $http, $state, store, jwtHelper, $stateParams) {
+  loginModalCtrl.factory('UsersApi', ['$http', 
+    function($http) {
       var service = {};
     
-      service.broadcast = function() {
-          $rootScope.$broadcast("UsersApiBroadcast");
-      };
-    
-      service.assignCurrentUser = function(token) {
-          store.set('jwt', token);
-          $rootScope.currentUser = jwtHelper.decodeToken(token);
-          return $rootScope.currentUser;
-      };
-      
       service.login = function(username, password) {
-        var d = $q.defer();
-        
-        setTimeout(function() {
-          $http({
-            url: 'http://localhost:5000/auth',
-            method: 'POST',
-            data: {"username": username, "password": password}
-          }).then(function(response) {
-            if (response.data.error) {
-              d.reject
-            } else {
-              service.assignCurrentUser(response.data.token);
-              d.resolve;
-            }
-          }, function(error) {
-            d.reject;
-          });
-        }, 100);
-
-        return d.promise;
+        return $http({
+          url: 'http://localhost:5000/auth',
+          method: 'POST',
+          data: {"username": username, "password": password}
+        });
       };
   
       return service;    
--- server/app/static/main.js
+++ server/app/static/main.js
@@ -27,28 +27,6 @@
     }
   ]);
   
-  app.run(['$rootScope', '$state', 'store', 'jwtHelper',  'loginModal',
-    function ($rootScope, $state, store, jwtHelper, loginModal) {
-      $state.transitionTo('select');
-
-      $rootScope.$on('$stateChangeStart', function(e, to) {
-        var requiresLogin = to.data && to.data.requiresLogin;
-        var noActiveToken = !store.get('jwt') || jwtHelper.isTokenExpired(store.get('jwt'));
-        if (requiresLogin && noActiveToken) {
-          e.preventDefault();
-          
-          loginModal()
-          .then(function () {
-            return $state.go(to.name);
-          })
-          .catch(function () {
-            return $state.go('select');
-          });
-        }
-      });
-    }
-  ]);
-  
   app.controller( 'AppCtrl', ['$scope', '$location', 
     function AppCtrl ( $scope, $location ) {
       $scope.$on('$routeChangeSuccess', function(e, nextRoute){
