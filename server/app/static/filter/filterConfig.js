49062e71314e2c2ea005739d8fdbea1d5bd951a4
Harry Payne <payne@nfdc.net>
2015-05-12 21:50:04 -0400

    Playing around, forgot to undo.

--- server/app/static/filter/filterConfig.js
+++ server/app/static/filter/filterConfig.js
@@ -17,7 +17,7 @@
         controllerAs: "filter",
         templateUrl: "/static/filter/filter.html",
         data: {
-          requiresLogin: true
+          requiresLogin: false
         }
       });
   };
