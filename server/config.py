394e1ed3d8fe33d585d453e525b650ba7ebbfc06
Harry Payne <payne@nfdc.net>
2015-11-13 13:09:45 -0500

    Mismatched <span> in template. Tiny cleanup in config.

--- server/config.py
+++ server/config.py
@@ -11,11 +11,8 @@ OPENID_PROVIDERS = [
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 #SQLALCHEMY_DATABASE_URI = "mysql://root:stormy@localhost/projects"
-
 SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://payne:stormy@localhost/projects"
-#SQLALCHEMY_DATABASE_URI = "mysql://root:stormy@localhost/create_all_test"
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 # mail server settings
